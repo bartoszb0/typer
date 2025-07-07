8df1e1b2b546e5054871196abaa949c30daea3b4
bartoszb0 <bartoszb2020@gmail.com>
2025-07-07 19:13:24 +0200

    added trimmed input and changing class of shown word

---
+++ src/words.js
@@ -0,0 +1,14 @@
+const words = [
+  "apple", "banana", "orange", "grape", "lemon",
+  "keyboard", "monitor", "laptop", "mouse", "screen",
+  "javascript", "function", "variable", "object", "array",
+  "quick", "brown", "fox", "jumps", "lazy",
+  "coding", "typing", "speed", "practice", "challenge",
+  "developer", "browser", "internet", "debug", "compile",
+  "syntax", "error", "logic", "script", "loop",
+  "react", "angular", "vue", "node", "express",
+  "random", "string", "input", "output", "test",
+  "clock", "time", "start", "stop", "reset"
+];
+
+export const randomWords = words.sort(() => Math.random() - 0.5)
